03cb402d403bd314e29b9dcdd784040c333dc641
Célian Loisel <77807956+celianloisel@users.noreply.github.com>
2024-03-28 14:43:46 -0700

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -58,16 +58,16 @@ define( 'ALLOW_UNFILTERED_UPLOADS', false );
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define( 'DB_NAME', 'french_f1_league' );
+define( 'DB_NAME', 'nobleffl' );
 
 /** Utilisateur de la base de données MySQL. */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'Noble' );
 
 /** Mot de passe de la base de données MySQL. */
 define( 'DB_PASSWORD', 'root' );
 
 /** Adresse de l’hébergement MySQL. */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'FFL-YZVZUZVDJSDIDBZOD' );
 
 /** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
